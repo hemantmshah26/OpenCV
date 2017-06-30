dd8cab9c58eec2c5e8a6e67b771b7118a33502ef
hemantmshah26 <hemant.m.shah@gmail.com>
2017-06-30 16:25:53 -0700

    Update Open a grayscale jpeg file.py

--- Open a grayscale jpeg file.py
+++ Open a grayscale jpeg file.py
@@ -13,3 +13,12 @@ img = cv2.imread('test.jpg',cv2.IMREAD_GRAYSCALE)
       cv2.imshow('image',img)
       cv2.waitKey(0)
       cv2.destroyAllWindows()
+# Above code helps open image in gray color, if you want to view image in Color, do this
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+# read image
+img = cv2.imread('watch.jpg', 1)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
